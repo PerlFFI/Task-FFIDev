c54452f0fdeb0272d68b47b99db68dc5f1893fa5
Graham Ollis <plicease@cpan.org>
2021-10-02 21:41:09 -0600

    initial implementation

--- lib/Task/FFIDev.pm
+++ lib/Task/FFIDev.pm
@@ -2,10 +2,76 @@ package Task::FFIDev;
 
 use strict;
 use warnings;
-use 5.008004;
+use 5.020;
 
 # ABSTRACT: Task bundle for FFI development
 # VERSION
+
+=head1 SYNOPSIS
+
+ $ cpanm Task::FFIDev
+
+=head1 DESCRIPTION
+
+This L<Task> bundle is useful for those doing FFI development with L<FFI::Platypus>.  Installing it
+will give you these modules:
+
+=over 4
+
+=item L<Dist::Zilla::MintingProfile::FFI>
+
+L<Dist::Zilla> minting profile for creating L<FFI::Platypus> bindings.
+
+=item L<Dist::Zilla::Plugin::FFI>
+
+L<Dist::Zilla> plugins useful for FFI
+
+=item L<FFI::C>
+
+Create interfaces to C structured data.
+
+=item L<FFI::CheckLib>
+
+Find dynamic libraries for use with FFI
+
+=item L<FFI::Platypus>
+
+Library for writing your own FFI bindings in perl
+
+=item L<FFI::Platypus::Type::Enum>
+
+Platypus type plugin for enumerated types
+
+=item L<FFI::Platypus::Type::PtrObject>
+
+Platypus type plugin for opaque pointer objects
+
+=item L<PeekPoke::FFI>
+
+Library for peeking and poking arbitrary memory locations.
+
+=item L<Test2::Tools::FFI>
+
+Testing tools for FFI.
+
+=back
+
+The latest versions as of when this L<Task> was released should be installed at minimum, if they are
+not already installed.
+
+Other prereqs may be added in the future if they are deemed useful for FFI development.
+
+=head1 CAVEATS
+
+This module does require Perl 5.20 or better currently, because at least some of its prereqs require
+that version.  Note that FFI bindings authored with these tools should work on Perls of at least
+5.8.4 or better, so this is just a I<development> requirement.
+
+This L<Task> indirectly requires L<Alien::FFI>.  If you do not want to build that from source
+or do not have internet access where the build is happening, you will want to pre-install libffi.
+On Debian based systems you can do that with C<sudo apt-get update && sudo apt-get install libffi-dev>.
+
+=cut
 
 1;
 
